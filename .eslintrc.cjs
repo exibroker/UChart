18b6563c1208bade13a32b8a9e83a23ae2c38e59
exibroker <exibroker@gmail.com>
2025-09-03 11:37:45 +0700

    update typescript latest version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: 'love',
+  extends: ['love'],
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off",
     '@typescript-eslint/space-before-function-paren': 'off',
